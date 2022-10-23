b0ea2066c45a9edb4913a641940810d95ee11e69
Carlos Henrique Miranda Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2022-10-23 11:22:36 -0300

    Update mecanica_basica3.cs

--- Segundo Modulo/mecanica_basica3.cs
+++ Segundo Modulo/mecanica_basica3.cs
@@ -17,6 +17,23 @@ public class CarroAnima : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    
+        /*
+        A depender do computador do usuário, a sprite pode ir
+        rapida demais ou devagar demais, tudo depende do FPS.
+        Para tornar a velocidade padrão para todos os computa-
+        dores, usamos o Time.deltaTime transforma os frames por
+        segundo em variáveis que podemos usar, assim 100FPS sig-
+        nifica que a cada 1s temos 100 frames, logo temos 1 frame
+        a cada 0,01 segundos, este 0,01 segundos é o valor retor-
+        nado pelo Time.deltaTime . Usando esse valor, podemos tor-
+        nar constante a velocidade da sprite em todos os tipos de 
+        PC. Por isso multipliquei pela variável novovlanteVel e 
+        novoYVel, também usei o volanteVel e yVel para não tornar
+        o movimento da sprite pesada, pense neles como 150 e 10 
+        respectivamente.
+        */
+        
         float novovolanteVel = Input.GetAxis("Horizontal") * volanteVel * Time.deltaTime;
         float novoYVel = Input.GetAxis("Vertical")* yVel * Time.deltaTime;
 
