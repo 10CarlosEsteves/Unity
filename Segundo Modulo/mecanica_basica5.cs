80ee89c91a3900f9d5975ddb30243cec9a241aef
Carlos Henrique Miranda Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2022-10-26 17:33:59 -0300

    Update mecanica_basica5.cs

--- Segundo Modulo/mecanica_basica5.cs
+++ Segundo Modulo/mecanica_basica5.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class CarroAnimacao : MonoBehaviour
 {
+    /*
+        Vamos criar um impulso, a depender da sprite
+        que o carro passa por cima, acionamos o boost
+        e esse boost aumenta velocidade do carro. Se
+        o nosso carro colidir com alguma coisa, ele 
+        fica lento.
+    */
 
+    //Adição das variaveis Lentidao e impulso
     [SerializeField] float Volante = 120f;
     [SerializeField] float Acelerador = 20f;
     [SerializeField] float Lentidao = 10f;
@@ -15,11 +23,14 @@ public class CarroAnimacao : MonoBehaviour
     {
         
     }
-
+    
+    //Se houver colisão, nossa velocidade fica lenta
     void OnCollisionEnter2D(Collision2D other) {
         Acelerador = Lentidao;
     }
 
+    //Se passarmos por algum gatilho e esse gatilho
+    //tiver a tag "Boost", então ganhamos impulso
     void OnTriggerEnter2D(Collider2D other) {
         if(other.tag=="Boost"){
             Acelerador= impulso;
