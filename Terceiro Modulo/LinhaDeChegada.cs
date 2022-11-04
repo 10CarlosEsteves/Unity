bc4a1d2fbcff2c43bc1ba1a136058cc290f0a710
Carlos Henrique Miranda Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2022-11-03 21:48:21 -0300

    Update LinhaDeChegada.cs

--- Terceiro Modulo/LinhaDeChegada.cs
+++ Terceiro Modulo/LinhaDeChegada.cs
@@ -8,6 +8,12 @@ using UnityEngine.SceneManagement;
 public class LinhadeChegada : MonoBehaviour{
     [SerializeField] float Delay=2f;
     [SerializeField] ParticleSystem particulasDaVitoria;
+    
+    /*
+    Como estamos interessados em apenas tocar uma música,
+    apenas ativamos ela pelo próprio AudioSource
+    */
+    
     AudioSource somDeVitoria;
     bool vitorioso = false;
 
