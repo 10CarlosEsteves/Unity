4cb9ed27e41b3c96c5a562f21601babf7a08fc1a
Carlos Henrique Miranda Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2022-11-03 21:42:36 -0300

    Update Detecte_Acidente2.cs

--- Terceiro Modulo/Detecte_Acidente2.cs
+++ Terceiro Modulo/Detecte_Acidente2.cs
@@ -8,10 +8,25 @@ using UnityEngine.UI;
 
 public class Detecte_Acidente : MonoBehaviour{
 
+  /*
+  Fizemos algumas modificações para dar ao jogo
+  as seguintes funcionalidades: Som ao se aciden-
+  tar e travar os controles do jogador, aqui apenas
+  manipulamos as variáveis para bloquear os controles
+  */
   
   [SerializeField] float Delay=2f;
   [SerializeField] SurfaceEffector2D superficie;
   [SerializeField] ParticleSystem sangramento;
+  
+  /*
+  Se quiséssimos que várias musicas fossem to-
+  cadas ao longo do jogo, então criaríamos vá-
+  rios AudioClip como o abaixo e então podería-
+  mos acessar cada um separadamente usando
+  GetComponent<AudioSource>().PlayOneShot(acidenteSFX);
+  */
+  
   [SerializeField] AudioClip acidenteSFX;
   bool seAcidentou = false;
 
@@ -29,7 +44,9 @@ public class Detecte_Acidente : MonoBehaviour{
         sangramento.Play();
         superficie.enabled=false;
         Debug.Log("Voce morreu!!!");
+        //Acessso ao método de outro script
         GetComponent<Controle_Jogador>().BloquearControles();
+        //Toque uma vez a música acidenteSFX
         GetComponent<AudioSource>().PlayOneShot(acidenteSFX);
         Invoke("Reiniciar", Delay);
     }
